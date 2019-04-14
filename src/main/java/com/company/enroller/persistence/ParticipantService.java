f009a9f61e196d1b42f88254bbc176fdeaf7306f
student23 <student23@pcoip.ki.agh.edu.pl>
2019-04-14 12:02:35 +0200

    first

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -19,4 +20,29 @@ public class ParticipantService {
 		return connector.getSession().createCriteria(Participant.class).list();
 	}
 
+	public Participant findByLogin(String login) {
+		return (Participant) connector.getSession().get(Participant.class, login);
+	}
+
+	public Participant add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+		return participant;
+		
+	}
+	public Participant delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+		return participant;
+		
+	}
+	public Participant update(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(participant);
+		transaction.commit();
+		return participant;
+		
+	}
 }
